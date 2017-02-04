af2b19cef1ecc3c5db0cd594d50db1b6afb635fb
ajarunjohn <ajarunjohn@gmail.com>
2017-02-04 14:30:39 +0530

    Update AVeryBigSum.java

--- AVeryBigSum.java
+++ AVeryBigSum.java
@@ -9,9 +9,14 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        long sum=0;
         int arr[] = new int[n];
         for(int arr_i=0; arr_i < n; arr_i++){
             arr[arr_i] = in.nextInt();
         }
+        for(int arr_i=0; arr_i < n; arr_i++){
+               sum=sum+arr[arr_i];
+        }
+        System.out.println(sum);
     }
 }
