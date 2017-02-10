658dea8846ee6b165a58077bc57a1760b8a939a8
ajarunjohn <ajarunjohn@gmail.com>
2017-02-10 12:51:03 +0530

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public static  Node insert(Node head,int data) {
+        //Complete this method
+          Node n = new Node(data);
+        Node temp = head;
+        if (head == null) {
+            head = n;
+            return head;
+        }
+ 
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+ 
+        temp.next = n;
+        return head;
+    }
