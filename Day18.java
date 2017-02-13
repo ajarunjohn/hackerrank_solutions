594101e56d39f00cabd7ddb94b16997415e0d137
ajarunjohn <ajarunjohn@gmail.com>
2017-02-13 14:02:09 +0530

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,25 @@
+public class Solution {
+    
+    LinkedList queue;
+    Stack stack;
+    
+    public Solution() {
+        queue = new LinkedList();
+        stack = new Stack();
+    }
+    
+    public void pushCharacter(char ch) {
+        stack.push(ch);
+    }
+    
+    public void enqueueCharacter(char ch) {
+        queue.addLast(ch);
+    }
+    
+    public char popCharacter() {
+        return (char) stack.pop();
+    }
+    
+    public char dequeueCharacter() {
+        return (char) queue.remove(0);
+    }
